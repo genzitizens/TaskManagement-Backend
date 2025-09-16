b0e94c63f36be569c7cb753b54fb8db7a8033b39
mediocreasian <exelbert2010@gmail.com>
2025-09-17 00:14:53 +0800

    Updated profile

--- src/test/java/com/task_management/TaskManagementApplicationTests.java
+++ src/test/java/com/task_management/TaskManagementApplicationTests.java
@@ -2,6 +2,7 @@ package com.task_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
