57a00fc94a853704603660c9edadfd2227f16804
Exel <53962638+mediocreasian@users.noreply.github.com>
2025-09-23 22:37:20 +0800

    Fix actuator test to avoid Prometheus dependency (#17)

--- src/test/java/com/task_management/monitoring/ActuatorIntegrationTest.java
+++ src/test/java/com/task_management/monitoring/ActuatorIntegrationTest.java
@@ -1,11 +1,11 @@
 package com.task_management.monitoring;
 
-import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -22,7 +22,7 @@ class ActuatorIntegrationTest {
     private MetricsEndpoint metricsEndpoint;
 
     @Autowired
-    private PrometheusMeterRegistry prometheusMeterRegistry;
+    private PrometheusScrapeEndpoint prometheusScrapeEndpoint;
 
     @Test
     void healthEndpointReportsUpStatus() {
@@ -36,7 +36,7 @@ class ActuatorIntegrationTest {
 
     @Test
     void prometheusRegistryExportsCustomCounter() {
-        String scrape = prometheusMeterRegistry.scrape();
+        String scrape = prometheusScrapeEndpoint.scrape();
         assertTrue(scrape.contains("task_management_tasks_created_total"));
     }
 }
